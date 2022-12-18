7e8b1a2b92810728fc88a61f4c340f6b69ebe0dc
Jason Heyer <heyerjt01@mounties.mansfield.edu>
2022-12-18 13:47:46 -0500

    add material tabs component

--- bass-app/src/app/app.module.ts
+++ bass-app/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatTabsModule} from '@angular/material/tabs';
+import { TabNavComponent } from './tab-nav/tab-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabNavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
