71d4ffa6a37f05ee95efbbbc4e875b8938730eaa
Jason Heyer <heyerjt01@mounties.mansfield.edu>
2023-01-21 18:05:21 -0500

    cleaning up

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { MatTabChangeEvent } from '@angular/material/tabs';
 import {ThemePalette} from '@angular/material/core';
 
 @Component({
@@ -9,12 +8,7 @@ import {ThemePalette} from '@angular/material/core';
 })
 export class AppComponent {
   title = 'JTH Bass Fishing';
-  // activeTabIndex = 0;
-  // selectedTabChange = 0;
-  // season = 'home' //Winter!! lol..
-
   background: ThemePalette = undefined;
-
   links = [
     {
       label: 'Home',
@@ -34,47 +28,12 @@ export class AppComponent {
     }
   ];
   activeLink = this.links[0].path;
-  constructor(){
-    // if (this.season == 'home'){
-    //   this.activeTabIndex = 0;
-    //   this.selectedTabChange = 0;
-    // }
-    // if (this.season == 'spring'){
-    //   this.activeTabIndex = 1;
-    //   this.selectedTabChange = 1;
-    // }
-    // if (this.season == 'summer'){
-    //   this.activeTabIndex = 2;
-    //   this.selectedTabChange = 2;
-    // }
-    // if (this.season == 'fall'){
-    //   this.activeTabIndex = 3;
-    //   this.selectedTabChange = 3;
-    // }
-  };
 
-  // changeSeason(tabChangeEvent: MatTabChangeEvent) {
-  //   switch (tabChangeEvent.index) {
-  //     case 0:
-  //       this.season = 'home';
-  //       this.activeTabIndex = 0;
-  //       this.selectedTabChange = 0;
-  //       break;
-  //     case 1:
-  //       this.season = 'spring';
-  //       this.activeTabIndex = 1;
-  //       this.selectedTabChange = 1;
-  //       break;
-  //     case 2:
-  //       this.season = 'summer';
-  //       this.activeTabIndex = 2;
-  //       this.selectedTabChange = 2;
-  //       break;
-  //     case 3:
-  //       this.season = 'fall';
-  //       this.activeTabIndex = 3;
-  //       this.selectedTabChange = 3;
-  //       break;
-  //   }
-  // }
+
+  /**
+   * No use for this really.. just reference for now...
+   */
+  toggleBackground() {
+    this.background = this.background ? undefined : 'primary';
+  }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,25 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatTabsModule} from '@angular/material/tabs';
-import { MaterialTabsComponent } from './material-tabs/material-tabs.component';
-import { AboutComponent } from './about/about.component';
-import { SpringContentComponent } from './spring-content/spring-content.component';
-import { SummerContentComponent } from './summer-content/summer-content.component';
-import { FallContentComponent } from './fall-content/fall-content.component';
-import { HomeContentComponent } from './home-content/home-content.component';
-import { AppBaitsComponent } from './app-baits/app-baits.component';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MaterialTabsComponent,
-    AboutComponent,
-    // SpringContentComponent,
-    // SummerContentComponent,
-    // FallContentComponent,
-    // HomeContentComponent,
-    AppBaitsComponent
   ],
   imports: [
     BrowserModule,
